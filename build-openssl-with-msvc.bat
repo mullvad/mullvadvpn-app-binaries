55b61939022f3fa535507c6d882d0a0a2c7485bd
Emīls Piņķis <emils@mullvad.net>
2018-07-04 14:11:49 +0100

    Move SSL config into a variable in build-openssl-with-msvc.bat

--- build-openssl-with-msvc.bat
+++ build-openssl-with-msvc.bat
@@ -1,19 +1,20 @@
 cd openssl
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-perl Configure VC-WIN64A ^
-  no-shared ^
-  --prefix=%cd%\..\msvc-openssl ^
-  --openssldir=%cd%\..\msvc-openssl ^
-  no-seed ^
-  no-cast ^
-  no-seed ^
+
+set OPENSSL_CONFIG =   enable-capieng ^
+  enable-rfc3779 ^
+  no-camellia ^
   no-cast ^
   no-dso ^
-  no-camellia ^
   no-idea ^
-  enable-rfc3779 ^
-  enable-capieng ^
+  no-seed ^
+  no-shared ^
   -FIWindows.h
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+perl Configure VC-WIN64A ^
+  --prefix=%cd%\..\msvc-openssl ^
+  --openssldir=%cd%\..\msvc-openssl ^
+  %OPENSSL_CONFIG%
+
 nmake clean
 nmake build_generated
 nmake build_libs_nodep
