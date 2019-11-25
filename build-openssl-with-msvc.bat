4603e9acde0d4eeddd8a03018931656632669438
Linus Färnstrand <linus@mullvad.net>
2019-11-25 11:27:28 +0100

    Explicitly copy the static versions of the OpenSSL libraries

--- build-openssl-with-msvc.bat
+++ build-openssl-with-msvc.bat
@@ -21,8 +21,8 @@ nmake build_generated
 nmake build_libs_nodep
 cd ..
 :: Copy the libraries
-copy /y openssl\libssl.lib x86_64-pc-windows-msvc\
-copy /y openssl\libcrypto.lib x86_64-pc-windows-msvc\
+copy /y openssl\libssl_static.lib x86_64-pc-windows-msvc\libssl.lib
+copy /y openssl\libcrypto_static.lib x86_64-pc-windows-msvc\libcrypto.lib
 :: Copy headers
 copy /y openssl\include\openssl\opensslconf.h x86_64-pc-windows-msvc\include\openssl\
 copy /y openssl\include\openssl\opensslv.h x86_64-pc-windows-msvc\include\openssl\
