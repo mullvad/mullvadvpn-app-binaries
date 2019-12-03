a9da8ec518ca2d7f295922ef9ec548b047e197ea
David Lönnhager <david.l@mullvad.net>
2019-12-03 18:21:56 +0100

    Fix msvc build configuration for OpenSSL

--- build-openssl-with-msvc.bat
+++ build-openssl-with-msvc.bat
@@ -1,8 +1,7 @@
 cd openssl
 
-set OPENSSL_CONFIG =   enable-capieng ^
+set OPENSSL_CONFIG=enable-capieng ^
      enable-rfc3779 ^
-     no-ssl2 ^
      no-ssl3 ^
      no-camellia ^
      no-cast ^
@@ -10,6 +9,7 @@ set OPENSSL_CONFIG =   enable-capieng ^
      no-idea ^
      no-shared ^
      no-seed
+
 perl Configure VC-WIN64A ^
   --prefix=%cd%\..\msvc-openssl ^
   --openssldir=%cd%\..\msvc-openssl ^
@@ -20,9 +20,11 @@ nmake clean
 nmake build_generated
 nmake build_libs_nodep
 cd ..
+
 :: Copy the libraries
-copy /y openssl\libssl_static.lib x86_64-pc-windows-msvc\libssl.lib
-copy /y openssl\libcrypto_static.lib x86_64-pc-windows-msvc\libcrypto.lib
+copy /y openssl\libssl.lib x86_64-pc-windows-msvc\libssl.lib
+copy /y openssl\libcrypto.lib x86_64-pc-windows-msvc\libcrypto.lib
+
 :: Copy headers
 copy /y openssl\include\openssl\opensslconf.h x86_64-pc-windows-msvc\include\openssl\
 copy /y openssl\include\openssl\opensslv.h x86_64-pc-windows-msvc\include\openssl\
