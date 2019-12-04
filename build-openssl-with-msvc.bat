8b787cce5da4538bd4567198ff90b805da6e88f7
David Lönnhager <david.l@mullvad.net>
2019-12-04 09:22:22 +0100

    Update indentation

--- build-openssl-with-msvc.bat
+++ build-openssl-with-msvc.bat
@@ -1,14 +1,14 @@
 cd openssl
 
 set OPENSSL_CONFIG=enable-capieng ^
-     enable-rfc3779 ^
-     no-ssl3 ^
-     no-camellia ^
-     no-cast ^
-     no-dso ^
-     no-idea ^
-     no-shared ^
-     no-seed
+  enable-rfc3779 ^
+  no-ssl3 ^
+  no-camellia ^
+  no-cast ^
+  no-dso ^
+  no-idea ^
+  no-shared ^
+  no-seed
 
 perl Configure VC-WIN64A ^
   --prefix=%cd%\..\msvc-openssl ^
