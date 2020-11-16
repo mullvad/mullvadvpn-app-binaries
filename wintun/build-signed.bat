e34860c7b36a213626575ac23a84fd45dc625080
David Lönnhager <david.l@mullvad.net>
2020-11-16 12:34:59 +0100

    Update Wintun and remove TAP driver

--- wintun/build-signed.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Use primary code signing certificate (CN = Amagicom AB).
-set SigningCertificate="%1"
-set SigningCertificatePassword="%2"
-set TimestampServer=http://timestamp.comodoca.com/?td=sha256
-call build.bat
