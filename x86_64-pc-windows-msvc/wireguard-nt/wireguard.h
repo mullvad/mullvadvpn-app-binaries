ef0517207aa341678fa1bd670edf0da459369b9e
David Lönnhager <david.l@mullvad.net>
2024-04-12 10:09:14 +0200

    Update wireguard-nt driver

--- x86_64-pc-windows-msvc/wireguard-nt/wireguard.h
+++ x86_64-pc-windows-msvc/wireguard-nt/wireguard.h
@@ -15,6 +15,8 @@
 extern "C" {
 #endif
 
+#include "../driver/daita.h"
+
 #ifndef ALIGNED
 #    if defined(_MSC_VER)
 #        define ALIGNED(n) __declspec(align(n))
@@ -229,7 +231,8 @@ typedef enum
     WIREGUARD_PEER_HAS_ENDPOINT = 1 << 3,             /**< The Endpoint field is set */
     WIREGUARD_PEER_REPLACE_ALLOWED_IPS = 1 << 5,      /**< Remove all allowed IPs before adding new ones */
     WIREGUARD_PEER_REMOVE = 1 << 6,                   /**< Remove specified peer */
-    WIREGUARD_PEER_UPDATE = 1 << 7                    /**< Do not add a new peer */
+    WIREGUARD_PEER_UPDATE = 1 << 7,                   /**< Do not add a new peer */
+    WIREGUARD_PEER_HAS_CONSTANT_PACKET_SIZE = 1 << 8  /**< The ConstantPacketSize field is set */
 } WIREGUARD_PEER_FLAG;
 
 typedef struct _WIREGUARD_PEER WIREGUARD_PEER;
@@ -245,6 +248,7 @@ struct ALIGNED(8) _WIREGUARD_PEER
     DWORD64 RxBytes;                         /**< Number of bytes received */
     DWORD64 LastHandshake;                   /**< Time of the last handshake, in 100ns intervals since 1601-01-01 UTC */
     DWORD AllowedIPsCount;                   /**< Number of allowed IP structs following this struct */
+    BOOLEAN ConstantPacketSize;              /**< Constant packet size. Smaller packets are padded up to the MTU */
 };
 
 typedef enum
@@ -300,6 +304,49 @@ BOOL(WINAPI WIREGUARD_GET_CONFIGURATION_FUNC)
 (_In_ WIREGUARD_ADAPTER_HANDLE Adapter,
  _Out_writes_bytes_all_(*Bytes) WIREGUARD_INTERFACE *Config,
  _Inout_ DWORD *Bytes);
+
+/**
+ * Enable DAITA for the given WireGuard device.
+ *
+ * @param Adapter            Adapter handle obtained with WireGuardCreateAdapter or WireGuardOpenAdapter
+ *
+ * @param EventsCapacity     Maximum number of events to store in the internal buffer.
+ *
+ * @param ActionsCapacity    Maximum number of actions to store in the internal buffer.
+ */
+typedef _Must_inspect_result_
+_Return_type_success_(return != FALSE)
+BOOL(WINAPI WIREGUARD_DAITA_ACTIVATE_FUNC)(_In_ WIREGUARD_ADAPTER_HANDLE Adapter, SIZE_T EventsCapacity, SIZE_T ActionsCapacity);
+
+/**
+ * Event object that is signaled when there are DAITA events to receive using WireGuardDaitaReceiveEvents.
+ */
+typedef _Must_inspect_result_
+_Return_type_success_(return != NULL)
+_Post_maybenull_
+HANDLE(WINAPI WIREGUARD_DAITA_EVENT_DATA_AVAILABLE_FUNC)(_In_ WIREGUARD_ADAPTER_HANDLE Adapter);
+
+/**
+ * Retrieves DAITA events.
+ *
+ * @param Events    A buffer that can contain DAITA_EVENT_BUFFER_CAPACITY events (DAITA_EVENT_RING_BUFFER_SIZE bytes).
+ *
+ * @return The function returns the number of events read into Events. If an error occurs, the return value
+ *         is zero and GetLastError returns the last error. If there are no items to read,
+ *         GetLastError returns ERROR_NO_MORE_ITEMS.
+ */
+typedef _Must_inspect_result_
+SIZE_T(WINAPI WIREGUARD_DAITA_RECEIVE_EVENTS_FUNC)
+(_In_ WIREGUARD_ADAPTER_HANDLE Adapter, _Inout_ DAITA_EVENT *Events);
+
+/* Sends a DAITA action.
+ *
+ * @return Iff the function succeeds, the return value is nonzero. To get extended error information, call GetLastError.
+ *         GetLastError returns ERROR_INSUFFICIENT_BUFFER is the underlying buffer is full.
+ */
+typedef _Must_inspect_result_
+_Return_type_success_(return != FALSE)
+BOOL(WINAPI WIREGUARD_DAITA_SEND_ACTION_FUNC)(_In_ WIREGUARD_ADAPTER_HANDLE Adapter, _In_ const DAITA_ACTION *Action);
 
 #pragma warning(pop)
 
